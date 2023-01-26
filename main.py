302c2a4999c3fda0129e9304c4e2761d1f6ff158
sacred70 <sacred70@mail.ru>
2023-01-26 23:25:32 +0700

    1

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-# This is a sample Python script.
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from functions import get_user, get_order, get_offer
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+app.url_map.strict_slashes = False
+
+db = SQLAlchemy(app)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class User(db.Model):
+    # Шаг №1: модель пользователя
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text(100))
+    last_name = db.Column(db.Text(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.Text(100))
+    role = db.Column(db.Text(10))
+    phone = db.Column(db.Text(20))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Order(db.Model):
+    # Шаг №1: модель заказа
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(200))
+    description = db.Column(db.Text(500))
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    address = db.Column(db.Text(200))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Offer(db.Model):
+    # Шаг №1: модель предложения
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
